caaf3d8ed38fed54baadf0bc098b8722f8c3bd96
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-05-12 14:29:09 +0200

    added server address field to settings

--- lib/src/settings_screen/settings_screen.dart
+++ lib/src/settings_screen/settings_screen.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
-
   static const routeName = "/settings";
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  TextEditingController _serverAddressController = TextEditingController();
+
+  void _saveSettings() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString("serverAddress", _serverAddressController.text);
+  }
+
+  void _loadSettings() async {
+    var prefs = await SharedPreferences.getInstance();
+    var _serverAddress = await prefs.getString("serverAddress") ?? "";
+    setState(() {
+      _serverAddressController.text = _serverAddress;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadSettings();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +38,39 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Settings"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("LOL"),
-        ],
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(5, 10, 5, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _serverAddressController,
+                minLines: 1,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("server address"),
+                  hintText: "ws://example.com",
+                ),
+                onSubmitted: (_) {
+                  _saveSettings();
+                },
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => _saveSettings(),
+                  child: Text("Save"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
