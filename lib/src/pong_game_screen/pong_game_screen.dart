5aa3f9630bd649b2894608b66c5f9990347cca62
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-05-16 14:23:56 +0200

    added on up and on down listeners

--- lib/src/pong_game_screen/pong_game_screen.dart
+++ lib/src/pong_game_screen/pong_game_screen.dart
@@ -16,14 +16,53 @@ class _PongGameScreenState extends State<PongGameScreen> {
       appBar: AppBar(
         title: Text("Pong LOL"),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("LOL"),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: SizedBox(
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Listener(
+                    onPointerDown: (details) => print("Up Down"),
+                    onPointerUp: (details) => print("Up Up"),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.arrow_upward,
+                        size: 100,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Flexible(
+                child: SizedBox(
+                  height: double.infinity,
+                  width: double.infinity,
+                  child: Listener(
+                    onPointerDown: (details) => print("Down Down"),
+                    onPointerUp: (details) => print("Down Up"),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.arrow_downward,
+                        size: 100,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
