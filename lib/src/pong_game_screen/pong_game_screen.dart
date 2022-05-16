129376ffb4fce10b341b3345f361737a2e6e1c0b
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2022-05-16 14:45:28 +0200

    EndOfLesson 2022-05-16-14-45

--- lib/src/pong_game_screen/pong_game_screen.dart
+++ lib/src/pong_game_screen/pong_game_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:game_app/src/ws_handeling/ws_connection.dart';
+import 'package:provider/provider.dart';
 
 class PongGameScreen extends StatefulWidget {
   const PongGameScreen({Key? key}) : super(key: key);
@@ -10,11 +12,36 @@ class PongGameScreen extends StatefulWidget {
 }
 
 class _PongGameScreenState extends State<PongGameScreen> {
+  late var ws = context.read<WsConnection>();
+
+  String player = "clientP1";
+
+  // --------------------
+
+  void upPressed() {
+    Map data = {
+      "sender": player,
+      "receiver": "server",
+      "timestamp": DateTime.now().microsecondsSinceEpoch.toString(),
+      "data": {
+        "action": "upPressed",
+      }
+    };
+  }
+
+  void upReleased() {}
+
+  // --------------------
+
+  void downPressed() {}
+
+  void downReleased() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pong LOL"),
+        title: Text("Pong"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -29,8 +56,8 @@ class _PongGameScreenState extends State<PongGameScreen> {
                   height: double.infinity,
                   width: double.infinity,
                   child: Listener(
-                    onPointerDown: (details) => print("Up Down"),
-                    onPointerUp: (details) => print("Up Up"),
+                    onPointerDown: (details) => upPressed(),
+                    onPointerUp: (details) => upReleased(),
                     child: ElevatedButton(
                       onPressed: () {},
                       child: Icon(
@@ -49,8 +76,8 @@ class _PongGameScreenState extends State<PongGameScreen> {
                   height: double.infinity,
                   width: double.infinity,
                   child: Listener(
-                    onPointerDown: (details) => print("Down Down"),
-                    onPointerUp: (details) => print("Down Up"),
+                    onPointerDown: (details) => downPressed(),
+                    onPointerUp: (details) => downReleased(),
                     child: ElevatedButton(
                       onPressed: () {},
                       child: Icon(
